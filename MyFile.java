c90cb150a30c5599a65f4e4d1562b7f5e2bf92a7
Henry Arnold <devharnold@gmail.com>
2024-09-25 14:57:24 +0300

    My first Java class

---
+++ MyFile.java
@@ -0,0 +1,8 @@
+public class MyFile {
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+        System.out.println("Welcome to Java Programming class." );
+        System.out.println("I hope you have fun!");
+    }
+}
+
